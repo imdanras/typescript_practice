9b49df369ad7722dcba5d8e7b7d02f16b7d12340
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-09-20 14:20:13 -0700

    practice

--- greeter.ts
+++ greeter.ts
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-console.log("DOM fully loaded and parsed");
+    console.log("DOM fully loaded and parsed");
 
-var name = document.getElementById('name');
+    var name = document.getElementById('name');
 
-class Student {
-    fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    class Student {
+        fullName: string;
+        constructor(public firstName, public middleInitial, public lastName) {
+            this.fullName = firstName + " " + middleInitial + " " + lastName;
+        }
     }
-}
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+    interface Person {
+        firstName: string;
+        lastName: string;
+    }
 
-function greeter(person:Person) {
-    return 'hello ' + person.firstName + ' ' + person.lastName;
-}
+    function greeter(person:Person) {
+        return 'hello ' + person.firstName + ' ' + person.lastName;
+    }
 
-var user = new Student("Jane", "M.", "User");
+    var user = new Student("Jane", "M.", "User");
 
-name.innerHTML = greeter(user);
+    name.innerHTML = greeter(user);
 
 //DOMContentLoaded ending parentheses
 });
